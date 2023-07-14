c38c90173ab876ea426e0246b9d6350d90267c9f
Edward Shanahan <edwardshanahan357@gmail.com>
2023-07-14 12:42:05 +0000

    Add Recipe and Comment Models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Publish"))
+
+CATEGORY = (
+    ("Breakfast", "Breakfast"),
+    ("Lunch", "Lunch"),
+    ("Dinner", "Dinner"),
+    ("Snack", "Snack")
+)
+
+
+class Recipe(models.Model):
+    """ Recipe Model """
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts")
+    image = CloudinaryField("image", default="placeholder")
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.TextField(choices=CATEGORY)
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    servings = models.IntegerField()
+    calories = models.IntegerField()
+    carbs = models.IntegerField()
+    protein = models.IntegerField()
+    fats = models.IntegerField()
+    ingredients = models.TextField()
+    method = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name="recipe_name", blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """ Comment Model """
+    post = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
