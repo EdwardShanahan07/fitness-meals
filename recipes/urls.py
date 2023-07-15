c16dbff281b0ae6f690988614f8295acfe6b5c54
Edward Shanahan <edwardshanahan357@gmail.com>
2023-07-15 18:34:28 +0000

    Add a list of shared recipes to recipes_list.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Index.as_view(), name="home")
+    path('', views.Index.as_view(), name="home"),
+    path('recipes/', views.Recipe_List.as_view(), name="recipes_list")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -19,3 +19,21 @@ class Index(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 3
+
+
+class Recipe_List(generic.ListView):
+    """
+        Render home page and display 
+        the 3 most recently created recipes
+    """
+    """
+        Code used is taken from Code Institute Blog Walkthrough porject
+        'I Think Therefore I Blog'
+    https://github.com/Code-Institute-Solutions/Django3blog/tree/master/
+    12_final_deployment
+    """
+
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'recipes_list.html'
+    paginate_by = 6
