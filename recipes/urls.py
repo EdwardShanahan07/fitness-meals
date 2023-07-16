3d48b16828a1061f7a8cba250a85c0fd16a23967
Edward Shanahan <edwardshanahan357@gmail.com>
2023-07-16 14:23:59 +0000

    Render recipe detail to recipe_detail.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Index.as_view(), name="home"),
-    path('recipes/', views.Recipe_List.as_view(), name="recipes_list")
+    path('recipes/', views.RecipeList.as_view(), name="recipes_list"),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -21,10 +21,9 @@ class Index(generic.ListView):
     paginate_by = 3
 
 
-class Recipe_List(generic.ListView):
+class RecipeList(generic.ListView):
     """
-        Render home page and display 
-        the 3 most recently created recipes
+        Render all shared recipes
     """
     """
         Code used is taken from Code Institute Blog Walkthrough porject
@@ -37,3 +36,25 @@ class Recipe_List(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'recipes_list.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked
+            }
+        )
