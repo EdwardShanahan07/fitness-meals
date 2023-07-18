ea05340f1b49dc60a2a387212fa4326371fef84a
Edward Shanahan <edwardshanahan357@gmail.com>
2023-07-18 21:07:52 +0000

    Add RecipeLike and add form to recipe_detail.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.Index.as_view(), name="home"),
     path('recipes/', views.RecipeList.as_view(), name="recipes_list"),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail")
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail"),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name="recipe_like")
 ]
